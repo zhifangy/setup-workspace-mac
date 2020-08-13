aaa4188bf9d77435b65aa8d1e109c4009fb6f738
Zhifang <zhifang.ye.fghm@gmail.com>
2020-08-13 13:35:12 -0700

    [ENH] Update FSL

--- 05-fsl.sh
+++ 05-fsl.sh
@@ -9,12 +9,9 @@ fi
 export FSLDIR=${SETUP_ROOT}/fsl
 
 # FSL
-wget https://fsl.fmrib.ox.ac.uk/fsldownloads/fsl-6.0.3-macOS_64.tar.gz
-mkdir ${FSLDIR} && tar -xzf fsl-6.0.3-macOS_64.tar.gz -C ${FSLDIR} --strip-components 1
-rm fsl-6.0.3-macOS_64.tar.gz
-# A quick and dirty fix for getting latest fsleyes
-sed -i'.bak' 's/fsleyes.*/fsleyes/' ${FSLDIR}/etc/fslconf/fslpython_environment.yml
-sed -i'.bak' 's/fslpy.*/fslpy/' ${FSLDIR}/etc/fslconf/fslpython_environment.yml
+wget https://fsl.fmrib.ox.ac.uk/fsldownloads/fsl-6.0.4-macOS_64.tar.gz
+mkdir ${FSLDIR} && tar -xzf fsl-6.0.4-macOS_64.tar.gz -C ${FSLDIR} --strip-components 1
+rm fsl-6.0.4-macOS_64.tar.gz
 bash ${FSLDIR}/etc/fslconf/post_install.sh
 ${FSLDIR}/fslpython/bin/conda clean -apy
 # Use newer version of MSM
