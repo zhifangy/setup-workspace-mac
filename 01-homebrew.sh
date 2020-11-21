f8da70da75040cefb0996255369ba27be2e61f3f
Zhifang Ye <zhifang.ye.fghm@gmail.com>
2020-11-20 19:58:32 -0800

    [FIX] Add dependencies in homebrew

--- 01-homebrew.sh
+++ 01-homebrew.sh
@@ -4,11 +4,14 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Install packages
+brew cask install osxfuse
 brew tap AdoptOpenJDK/openjdk
 brew install wget vim curl cmake gcc llvm tcl-tk htop tree git libssh2 libgit2 node pandoc hdf5 \
     open-mpi openblas swig sshfs autossh imagemagick netpbm v8 openjdk
 
 # Install R
+brew cask install xquartz
+brew cask install adoptopenjdk
 brew tap sethrfore/homebrew-r-srf
 brew install -f -s sethrfore/r-srf/cairo
 brew install -s sethrfore/r-srf/r --with-openblas --with-java --with-texinfo --with-cairo --with-libtiff --with-icu4c
