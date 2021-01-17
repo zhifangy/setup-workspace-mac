3732d8acf25995db922c1c3dcffef0f7b1e495c2
Zhifang <zhifang.ye.fghm@gmail.com>
2021-01-16 21:49:02 -0800

    [ENH] Update basic packages installation

--- 01-homebrew.sh
+++ 01-homebrew.sh
@@ -7,11 +7,18 @@
 brew cask install osxfuse
 brew tap AdoptOpenJDK/openjdk
 brew install wget vim curl cmake gcc llvm tcl-tk htop tree git libssh2 libgit2 node pandoc hdf5 \
-    open-mpi openblas swig sshfs autossh imagemagick netpbm v8 openjdk
+    open-mpi openblas swig autossh imagemagick netpbm v8 openjdk
 
 # Install R
 brew cask install xquartz
 brew cask install adoptopenjdk
 brew tap sethrfore/homebrew-r-srf
-brew install -f -s sethrfore/r-srf/cairo
-brew install -s sethrfore/r-srf/r --with-openblas --with-java --with-texinfo --with-cairo --with-libtiff --with-icu4c
+brew tap sethrfore/homebrew-extras
+brew install -s sethrfore/r-srf/cairo-x11
+brew install tcl-tk-x11
+brew install -s sethrfore/r-srf/r --with-icu4c --with-libtiff --with-openblas --with-openjdk --with-tcl-tk-x11 --with-cairo-x11 --with-texinfo
+
+echo "
+In order to use sshfs, you need to install osxfuse manually.
+Then run brew install sshfs
+"
