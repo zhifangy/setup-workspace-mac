f30fdffc6c9db4725502964665e06df8a49fd6d7
Zhifang <zhifang.ye.fghm@gmail.com>
2021-01-20 15:20:27 -0800

    [ENH] Formatting

--- 04-r_environment.sh
+++ 04-r_environment.sh
@@ -19,41 +19,41 @@ R_LIBS <- Sys.getenv("R_LIBS")
 N_CPUS <- Sys.getenv("N_CPUS")
 
 install.packages(
-    c(
-        "devtools", "pacman", "fs", "here", "rprojroot", "styler", "feather", "processx", "languageserver",
-        "tidyverse", "vroom", "broom", "janitor", "reticulate", "jsonlite",
-        "data.table", "knitr", "kableExtra", "rmarkdown", "formatR", "printr",
-        "dbplyr", "DBI", "RSQLite", "lintr", "Rmpi"
-    ),
-    lib = R_LIBS, repos = "https://cloud.r-project.org",
-    clean = TRUE, Ncpus = N_CPUS
+  c(
+    "devtools", "pacman", "fs", "here", "rprojroot", "styler", "feather", "processx", "languageserver",
+    "tidyverse", "vroom", "broom", "janitor", "reticulate", "jsonlite",
+    "data.table", "knitr", "kableExtra", "rmarkdown", "formatR", "printr",
+    "dbplyr", "DBI", "RSQLite", "lintr", "Rmpi"
+  ),
+  lib = R_LIBS, repos = "https://cloud.r-project.org",
+  clean = TRUE, quiet = TRUE, Ncpus = N_CPUS
 )
 
 install.packages(
-    c(
-        "ggforce", "ggpmisc", "ggExtra", "ggfortify", "ggthemes", "ggsci", "paletteer",
-        "GGally", "ggridges", "ggraph", "lemon", "cowplot", "ggpubr", "corrplot", "ggbeeswarm",
-        "ggThemeAssist", "shades", "visreg", "magick", "tidygraph", "wesanderson"
-    ),
-    lib = R_LIBS, repos = "https://cloud.r-project.org",
-    clean = TRUE, Ncpus = N_CPUS
+  c(
+    "ggforce", "ggpmisc", "ggExtra", "ggfortify", "ggthemes", "ggsci", "paletteer",
+    "GGally", "ggridges", "ggraph", "lemon", "cowplot", "ggpubr", "corrplot", "ggbeeswarm",
+    "ggThemeAssist", "shades", "visreg", "magick", "tidygraph", "wesanderson"
+  ),
+  lib = R_LIBS, repos = "https://cloud.r-project.org",
+  clean = TRUE, quiet = TRUE, Ncpus = N_CPUS
 )
 
 install.packages(
-    c(
-        "psych", "afex", "emmeans", "coin", "robustbase", "Rmisc", "lme4", "lmerTest",
-        "mediation", "NPC", "jmv", "sjPlot", "finalfit", "cocor", "skimr",
-        "neurobase", "RNifti", "R.matlab",
-        "vitae", "rorcid"
-    ),
-    lib = R_LIBS, repos = "https://cloud.r-project.org",
-    clean = TRUE, Ncpus = N_CPUS
+  c(
+    "psych", "afex", "emmeans", "coin", "robustbase", "Rmisc", "lme4", "lmerTest",
+    "mediation", "NPC", "jmv", "sjPlot", "finalfit", "cocor", "skimr",
+    "neurobase", "RNifti", "R.matlab",
+    "vitae", "rorcid"
+  ),
+  lib = R_LIBS, repos = "https://cloud.r-project.org",
+  clean = TRUE, quiet = TRUE, Ncpus = N_CPUS
 )
 
 install.packages(
-    c("tidyverse", "afex", "phia", "snow", "nlme", "lme4", "paran", "psych", "brms", "corrplot", "metafor"),
-    lib = R_LIBS, repos = "https://cloud.r-project.org",
-    clean = TRUE, Ncpus = N_CPUS
+  c("phia", "snow", "nlme", "paran", "brms", "metafor"),
+  lib = R_LIBS, repos = "https://cloud.r-project.org",
+  clean = TRUE, quiet = TRUE, Ncpus = N_CPUS
 )
 '
 
