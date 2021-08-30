925563bc7a866230f490f1789d80068a76099f9a
Zhifang <zhifang.ye.fghm@gmail.com>
2021-08-30 14:01:30 -0700

    [ENH] Use poetry to manage python packages

--- 03-miniconda.sh
+++ 03-miniconda.sh
@@ -16,3 +16,21 @@ rm -f Miniconda3-latest-MacOSX-x86_64.sh
 conda config --prepend channels conda-forge
 conda update -yq conda
 conda update -yq --all
+
+# Poetry
+export POETRY_HOME=${SETUP_ROOT}/poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -
+unset POETRY_HOME
+
+# Add following lines into .zshrc
+echo "
+Add following lines to .zshrc:
+export PATH=${SETUP_ROOT}/poetry/bin:\${PATH}
+export POETRY_CACHE_DIR=${SETUP_ROOT}/poetry
+plugins(poetry)
+
+Execute following lines:
+source ~/.zshrc
+mkdir -p \${ZSH_CUSTOM}/plugins/poetry
+poetry completions zsh > \${ZSH_CUSTOM}/plugins/poetry/_poetry
+"
--- 04-python_environment.sh
+++ 04-python_environment.sh
@@ -10,7 +10,6 @@ echo "Current conda: $(which conda)"
 
 # Add channels
 conda config --add channels conda-forge
-conda config --add channels pytorch
 conda config --set channel_priority flexible
 
 # Environment name
@@ -24,104 +23,14 @@ conda create -p ${ENV_PREFIX} -y python=3.8
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate ${ENV_PREFIX}
 
-# Install packages using conda
-# Note:
-# - Pin numpy=1.19, since several packages are incompatible with numpy 1.20.
-# - Checklist: tensorflow, brainiak, yellowbrick
-conda install -yq \
-    "numpy=1.19" \
-    pandas \
-    scipy \
-    statsmodels \
-    scikit-learn \
-    scikit-image \
-    xgboost \
-    ipython \
-    jupyterlab \
-    jupyterlab-lsp \
-    jupyter-lsp-python \
-    xeus-python \
-    jupyterlab_code_formatter \
-    matplotlib \
-    seaborn \
-    plotly \
-    python-kaleido \
-    bokeh \
-    ipywidgets \
-    mpi4py \
-    h5py \
-    pybind11 \
-    flake8 \
-    autopep8 \
-    black \
-    yapf \
-    pytest \
-    jupytext \
-    nbdime \
-    pyjanitor \
-    python-dotenv \
-    pyprojroot \
-    memory_profiler \
-    threadpoolctl \
-    cookiecutter \
-    pytorch \
-    torchvision \
-    cudatoolkit \
-    nilearn \
-    nipype \
-    nitime \
-    pydicom \
-    dcmstack \
-    heudiconv \
-    umap-learn \
-    gensim \
-    numexpr \
-    pyrsistent \
-    pint \
-    py4j \
-    s3fs \
-    ipyvolume \
-    datalad
-
-# Install vtk, mayavi
-# Note:
-# - conda-forge version conflicts with lots of packages
-pip install -q --no-cache-dir vtk==9.0.1
-pip install -q --no-cache-dir mayavi
-# Install packages using pip
-# Note:
-pip install -q --no-cache-dir \
-    lets-plot \
-    timm \
-    yellowbrick \
-    rpy2 \
-    radian \
-    bigmpi4py \
-    pymanopt \
-    pybids \
-    bidscoin \
-    antspyx \
-    brainiak \
-    brainspace \
-    neuropythy \
-    pymvpa2 \
-    mne \
-    visualqc
-# Install hypertools
-# version 0.7.0 requires scikit-learn<0.24
-pip install -q --no-cache-dir ppca
-pip install -q --no-cache-dir --no-deps hypertools
-# pymer4
-# Note:
-# - Require old version of pandas due to deepdish
-# - Manually install missing dependency
-pip install -q --no-cache-dir deepdish
-pip install -q --no-cache-dir --no-deps pymer4
+# Install packages using poetry
+POETRY_CACHE_DIR=${SETUP_ROOT}/poetry
+cd "$(dirname "$0")"
+poetry install -v
 # Thingsvision
 # Note:
 # - Hard pin lots of packages
 # - Manually install missing dependency
-pip install -q --no-cache-dir ftfy
 pip install -q --no-cache-dir --no-deps thingsvision
 
 # Cleanup
@@ -129,4 +38,3 @@ conda clean -apy
 jupyter lab clean
 
 echo "Installation completed!"
-echo "Add 'numpy<1.20' into pyenv/conda-meta/pinned file."
