e86f22a11cf895e8d707f072bb7a12113cfd046f
Zhifang Ye <zhifang.ye.fghm@gmail.com>
2021-11-02 17:15:11 -0700

    [ENH] Remove osxfuse, use macfuse [FIX] Replace sshfs [FIX] Edit R's default Makeconf file [ENH] Add information of using llvm complier

--- 01-basics.sh
+++ 01-basics.sh
@@ -4,10 +4,10 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Install packages
-brew install --cask osxfuse
 brew tap AdoptOpenJDK/openjdk
 brew install wget vim curl cmake gcc llvm tcl-tk htop tree git libssh2 libgit2 node pandoc hdf5 \
-    open-mpi openblas swig sshfs autossh imagemagick netpbm v8 openjdk
+    open-mpi openblas swig autossh imagemagick netpbm v8 openjdk macfuse
+brew install gromgit/fuse/sshfs-mac
 
 # Install R
 brew install --cask xquartz
@@ -16,11 +16,17 @@ brew tap sethrfore/homebrew-r-srf
 brew install -s sethrfore/r-srf/cairo-x11
 brew install sethrfore/r-srf/tcl-tk-x11
 brew install -s sethrfore/r-srf/r --with-icu4c --with-libtiff --with-openblas --with-openjdk --with-tcl-tk-x11 --with-cairo-x11 --with-texinfo
+# Fix compilation executable for objective c++
+sed -i "" "s/OBJCXX =.*/OBJCXX = clang++/" $(brew --prefix r)/lib/R/etc/Makeconf
 
 # Install ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "
-In order to use sshfs, you need to install osxfuse manually.
-Then run brew install sshfs
+Add following line to .zshrc
+
+# Compilation environment
+export PATH="/usr/local/opt/llvm/bin:\${PATH}"
+export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib \$LDFLAGS"
+export CPPFLAGS="-I/usr/local/opt/llvm/include \$CPPFLAGS"
 "
