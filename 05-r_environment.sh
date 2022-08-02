32e1946c5b6e203c406d5a775372f93c31f43850
Zhifang <zhifang.ye.fghm@gmail.com>
2022-08-01 17:01:32 -0700

    [FIX] Revert unintentional comment

--- 05-r_environment.sh
+++ 05-r_environment.sh
@@ -31,96 +31,96 @@ install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
     formatR \
     feather \
     reticulate
-# # tidyverse
-# install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
-#     tidyverse \
-#     devtools \
-#     rmarkdown \
-#     BiocManager \
-#     vroom \
-#     gert
-# # dplyr database backends
-# install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
-#     arrow \
-#     dbplyr \
-#     DBI \
-#     dtplyr \
-#     duckdb \
-#     nycflights13 \
-#     Lahman \
-#     RMariaDB \
-#     RPostgres \
-#     RSQLite \
-#     fst
-# # report & publish
-# install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
-#     blogdown \
-#     bookdown \
-#     distill \
-#     rticles \
-#     rmdshower \
-#     xaringan \
-#     printr \
-#     kableExtra
-# # graphics
-# install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
-#     ggforce \
-#     ggpmisc \
-#     ggExtra \
-#     ggfortify \
-#     ggtext \
-#     gghighlight \
-#     ggthemes \
-#     ggsci \
-#     paletteer \
-#     GGally \
-#     ggridges \
-#     ggraph \
-#     lemon \
-#     cowplot \
-#     ggpubr \
-#     corrplot \
-#     ggbeeswarm \
-#     ggThemeAssist \
-#     shades \
-#     visreg \
-#     magick \
-#     tidygraph \
-#     wesanderson \
-#     MetBrewer \
-#     showtext
-# Rscript -e "remotes::install_github('jorvlan/raincloudplots', lib='${R_LIBS}', clean=TRUE, quiet=TRUE)"
-# # statistic
-# install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
-#     afex \
-#     emmeans \
-#     psych \
-#     coin \
-#     robustbase \
-#     Rmisc \
-#     lme4 \
-#     lmerTest \
-#     mediation \
-#     jmv \
-#     sjPlot \
-#     finalfit \
-#     cocor \
-#     skimr
-# # other
-# install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
-#     Rmpi \
-#     vitae \
-#     rorcid \
-#     RNifti \
-#     R.matlab
-# # packages for afni
-# install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
-#     phia \
-#     snow \
-#     nlme \
-#     paran \
-#     brms \
-#     metafor
+# tidyverse
+install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
+    tidyverse \
+    devtools \
+    rmarkdown \
+    BiocManager \
+    vroom \
+    gert
+# dplyr database backends
+install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
+    arrow \
+    dbplyr \
+    DBI \
+    dtplyr \
+    duckdb \
+    nycflights13 \
+    Lahman \
+    RMariaDB \
+    RPostgres \
+    RSQLite \
+    fst
+# report & publish
+install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
+    blogdown \
+    bookdown \
+    distill \
+    rticles \
+    rmdshower \
+    xaringan \
+    printr \
+    kableExtra
+# graphics
+install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
+    ggforce \
+    ggpmisc \
+    ggExtra \
+    ggfortify \
+    ggtext \
+    gghighlight \
+    ggthemes \
+    ggsci \
+    paletteer \
+    GGally \
+    ggridges \
+    ggraph \
+    lemon \
+    cowplot \
+    ggpubr \
+    corrplot \
+    ggbeeswarm \
+    ggThemeAssist \
+    shades \
+    visreg \
+    magick \
+    tidygraph \
+    wesanderson \
+    MetBrewer \
+    showtext
+Rscript -e "remotes::install_github('jorvlan/raincloudplots', lib='${R_LIBS}', clean=TRUE, quiet=TRUE)"
+# statistic
+install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
+    afex \
+    emmeans \
+    psych \
+    coin \
+    robustbase \
+    Rmisc \
+    lme4 \
+    lmerTest \
+    mediation \
+    jmv \
+    sjPlot \
+    finalfit \
+    cocor \
+    skimr
+# other
+install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
+    Rmpi \
+    vitae \
+    rorcid \
+    RNifti \
+    R.matlab
+# packages for afni
+install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} \
+    phia \
+    snow \
+    nlme \
+    paran \
+    brms \
+    metafor
 
 echo ${R_LIBS} > ${R_LIBS}/path
 
