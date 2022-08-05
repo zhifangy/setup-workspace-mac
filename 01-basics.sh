31e741f24774d1cf7a2b4c7b9950f50a7977d29a
Zhifang Ye <zhifang.ye.fghm@gmail.com>
2022-08-04 18:36:15 -0700

    [ENH] Add system pacckage lz4

--- 01-basics.sh
+++ 01-basics.sh
@@ -6,7 +6,8 @@
 # Install packages
 brew tap AdoptOpenJDK/openjdk
 brew install wget vim curl cmake gcc llvm tcl-tk htop tree git libssh2 libgit2 node pandoc hdf5 \
-    open-mpi openblas swig autossh imagemagick netpbm v8 openjdk macfuse harfbuzz fribidi mariadb-connector-c
+    open-mpi openblas swig autossh imagemagick netpbm v8 openjdk macfuse harfbuzz fribidi \
+    mariadb-connector-c lz4
 brew install gromgit/fuse/sshfs-mac
 
 # Install R
