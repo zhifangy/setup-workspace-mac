5d0462c3e788b0b2181862215c124ff381113093
Zhifang Ye <zhifang.ye.fghm@gmail.com>
2022-08-04 17:00:50 -0700

    [ENH] Add system package mariadb-connector-c

--- 01-basics.sh
+++ 01-basics.sh
@@ -6,7 +6,7 @@
 # Install packages
 brew tap AdoptOpenJDK/openjdk
 brew install wget vim curl cmake gcc llvm tcl-tk htop tree git libssh2 libgit2 node pandoc hdf5 \
-    open-mpi openblas swig autossh imagemagick netpbm v8 openjdk macfuse harfbuzz fribidi
+    open-mpi openblas swig autossh imagemagick netpbm v8 openjdk macfuse harfbuzz fribidi mariadb-connector-c
 brew install gromgit/fuse/sshfs-mac
 
 # Install R
