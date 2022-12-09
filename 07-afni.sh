57a30a4bee9e5311b36d5b75ec85fff484c3b931
Zhifang <zhifang.ye.fghm@gmail.com>
2022-12-09 14:03:36 -0800

    [ENH] Cleanup old installation during AFNI install process

--- 07-afni.sh
+++ 07-afni.sh
@@ -9,6 +9,28 @@ fi
 AFNI_DIR=${SETUP_ROOT}/afni
 PATH=${AFNI_DIR}:${PATH}
 
+# Check and cleanup old installation
+if [ -d ${AFNI_DIR} ]
+then
+    echo "Cleanup old AFNI installation ..."
+    rm -rf ${AFNI_DIR}
+fi
+if [ -d ~/.afni/help ]
+then
+    echo "Cleanup old AFNI help files ..."
+    rm -rf ~/.afni/help
+fi
+if [ -f ~/.afnirc ]
+then
+    echo "Cleanup old AFNI rc files ..."
+    rm ~/.afnirc
+fi
+if [ -f ~/.sumarc ]
+then
+    echo "Cleanup old SUMA rc files ..."
+    rm ~/.sumarc
+fi
+
 # AFNI
 curl -O https://afni.nimh.nih.gov/pub/dist/bin/misc/@update.afni.binaries
 tcsh @update.afni.binaries -package macos_10.12_local -bindir ${AFNI_DIR} -apsearch yes
@@ -16,6 +38,12 @@ rm @update.afni.binaries
 cp ${AFNI_DIR}/AFNI.afnirc ${HOME}/.afnirc
 suma -update_env
 apsearch -update_all_afni_help
+
+# Post installation cleanup
+if [ -f .R.Rout ]
+then
+    rm .R.Rout
+fi
 
 # Add following lines into .zshrc
 echo "
