a2660975d7bdbc4a275729735f1379435d950490
Zhifang <zhifang.ye.fghm@gmail.com>
2023-01-27 10:49:31 -0800

    [ENH] Update ANTs to 2.4.3

--- 09-ants.sh
+++ 09-ants.sh
@@ -7,10 +7,18 @@ fi
 
 # Setup
 ANTS_DIR=${SETUP_ROOT}/ants
+ANTS_VERSION=2.4.3
+
+# Check and cleanup old installation
+if [ -d ${ANTS_DIR} ]
+then
+    echo "Cleanup old ANTs installation ..."
+    rm -rf ${ANTS_DIR}
+fi
 
 # ANTs
 mkdir -p ${ANTS_DIR}
-wget -O- https://github.com/ANTsX/ANTs/releases/download/v2.4.2/ants-2.4.2-macos-12-X64-clang.zip \
+wget -O- https://github.com/ANTsX/ANTs/releases/download/v${ANTS_VERSION}/ants-${ANTS_VERSION}-macos-12-X64-clang.zip \
     | bsdtar -xf - -C ${ANTS_DIR} --strip-components 1
 chmod +x ${ANTS_DIR}/bin/*
 
