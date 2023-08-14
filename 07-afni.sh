b7910e0cde143809c56f70b741b5b518332b286b
Zhifang <zhifang.ye.fghm@gmail.com>
2023-08-14 12:14:55 -0700

    [ENH] Make sure AFNI dependencies are installed in this script [ENH] Optimize installation script

--- 07-afni.sh
+++ 07-afni.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
+set -e
 
-if [ -z ${SETUP_ROOT} ]
-then
+if [ -z ${SETUP_ROOT} ]; then
     source envs
 fi
 
 # Setup
 AFNI_DIR=${SETUP_ROOT}/afni
 PATH=${AFNI_DIR}:${PATH}
+# r
+R_LIBS=${R_LIBS:-${SETUP_ROOT}/renv}
+CRAN=${CRAN:-https://packagemanager.posit.co/cran/latest}
+N_CPUS=${N_CPUS:-4}
+PATH=${R_LIBS}/littler/examples:${R_LIBS}/littler/bin:${PATH}
+# set tbb related environment variable (for brms dependency RcppParallel)
+export TBB_INC=$(ls -d /usr/local/Cellar/tbb/*)/include
+export TBB_LIB=$(ls -d /usr/local/Cellar/tbb/*)/lib
 
 # Check and cleanup old installation
-if [ -d ${AFNI_DIR} ]
-then
-    echo "Cleanup old AFNI installation ..."
+if [ -d ${AFNI_DIR} ]; then
+    echo "Cleanup old AFNI installation..."
     rm -rf ${AFNI_DIR}
 fi
-if [ -d ~/.afni/help ]
-then
+if [ -d ~/.afni/help ]; then
     echo "Cleanup old AFNI help files ..."
     rm -rf ~/.afni/help
 fi
-if [ -f ~/.afnirc ]
-then
+if [ -f ~/.afnirc ]; then
     echo "Cleanup old AFNI rc files ..."
     rm ~/.afnirc
 fi
-if [ -f ~/.sumarc ]
-then
+if [ -f ~/.sumarc ]; then
     echo "Cleanup old SUMA rc files ..."
     rm ~/.sumarc
 fi
+
+# Install dependency via homebrew
+brew install netpbm cmake gfortran
+brew install --cask xquartz
+# Install R package dependency (list from rPkgsInstall.R)
+install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} -s \
+    afex phia snow nlme lmerTest paran psych brms corrplot metafor
 
 # AFNI
 curl -O https://afni.nimh.nih.gov/pub/dist/bin/misc/@update.afni.binaries
@@ -38,10 +49,13 @@ rm @update.afni.binaries
 cp ${AFNI_DIR}/AFNI.afnirc ${HOME}/.afnirc
 suma -update_env
 apsearch -update_all_afni_help
+# set options for X11
+defaults write org.macosforge.xquartz.X11 wm_ffm -bool true
+defaults write org.x.X11 wm_ffm -bool true
+defaults write com.apple.Terminal FocusFollowsMouse -string YES
 
 # Post installation cleanup
-if [ -f .R.Rout ]
-then
+if [ -f .R.Rout ]; then
     rm .R.Rout
 fi
 
@@ -50,5 +64,8 @@ echo "
 Add following line to .zshrc
 # AFNI
 export PATH=${AFNI_DIR}:\${PATH}
-export DYLD_LIBRARY_PATH=\${DYLD_LIBRARY_PATH}:/opt/X11/lib/flat_namespace
+
+# for RcppParallel (dependency of brms)
+export TBB_INC=\$(ls -d /usr/local/Cellar/tbb/*)/include
+export TBB_LIB=\$(ls -d /usr/local/Cellar/tbb/*)/lib
 "
