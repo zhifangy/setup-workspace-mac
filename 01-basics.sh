057be349faf030924bbce1daeea829f1acd58ca5
Zhifang <zhifang.ye.fghm@gmail.com>
2023-08-14 12:23:58 -0700

    [ENH] Reduce packages installed by homebrew [ENH] Move zsh plugin installation in this script [ENH] Remove custom R installation (move to script with R packages) [ENH] Better output message

--- 01-basics.sh
+++ 01-basics.sh
@@ -1,33 +1,39 @@
 #! /bin/bash
+set -e
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Install packages
-brew tap AdoptOpenJDK/openjdk
-brew install wget vim curl cmake gcc llvm tcl-tk htop tree git libssh2 libgit2 node pandoc hdf5 \
-    open-mpi openblas swig autossh imagemagick netpbm v8 openjdk macfuse harfbuzz fribidi \
-    mariadb-connector-c lz4 bat lsd orbstack
-brew install gromgit/fuse/sshfs-mac
+# basic
+brew install wget curl vim cmake gcc llvm tcl-tk libssh2 open-mpi openblas node swig v8 hdf5 imagemagick
+# useful utilities
+brew install git htop tree pandoc autossh bat lsd thefuck orbstack
+brew install gromgit/fuse/sshfs-mac macfuse
 
-# Install R
-brew install --cask xquartz
-brew install --cask adoptopenjdk
-brew tap sethrfore/homebrew-r-srf
-brew install -s sethrfore/r-srf/cairo-x11
-brew install sethrfore/r-srf/tcl-tk-x11
-brew install -s sethrfore/r-srf/r --with-icu4c --with-libtiff --with-openblas --with-openjdk --with-tcl-tk-x11 --with-cairo-x11 --with-texinfo
-# Fix compilation executable for objective c++
-sed -i "" "s/OBJCXX =.*/OBJCXX = clang++/" $(brew --prefix r)/lib/R/etc/Makeconf
-
-# Install ZSH
+# Install on-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install zsh plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo "
 Add following line to .zshrc
 
-# Compilation environment
-export PATH="/usr/local/opt/llvm/bin:\${PATH}"
-export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib \$LDFLAGS"
-export CPPFLAGS="-I/usr/local/opt/llvm/include \$CPPFLAGS"
+Modify .zshrc:
+1.Plugins
+    plugins=(git brew z zsh-autosuggestions zsh-syntax-highlighting)
+2.Themes
+    ZSH_THEME="powerlevel10k/powerlevel10k"
+3.Fonts
+    In order to use the powerlevel10k theme, you need to use fonts from the Nerd-fonts project
+    https://github.com/ryanoasis/nerd-fonts
+4. More alias
+    alias ll="lsd -l"
+    alias la="lsd -a"
+    alias lla="lsd -la"
+    alias lt="lsd --tree"
+5. More good stuff
+    eval $(thefuck --alias)
 "
