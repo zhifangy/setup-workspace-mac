d3c9d6c965751e212f0c98d1f1812af189f33f29
Zhifang <zhifang.ye.fghm@gmail.com>
2023-08-25 14:45:41 -0700

    [ENH] Install R packages from spec file

--- script/install_r_environment.sh
+++ script/install_r_environment.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ -z ${SETUP_ROOT} ]; then source $( dirname -- "$( readlink -f -- "$0"; )"; )/../envs; fi
 # Setup
+source $( dirname -- "$( readlink -f -- "$0"; )"; )/../envs
+SCRIPT_DIR=$( dirname -- "$( readlink -f -- "$0"; )"; )
+N_CPUS=${N_CPUS:-6}
 export R_LIBS=${SETUP_ROOT}/renv
 CRAN=${CRAN:-https://packagemanager.posit.co/cran/latest}
-N_CPUS=${N_CPUS:-4}
 
 # Install R from homebrew
 brew install r
@@ -15,113 +16,58 @@ brew install libgit2 libpng tbb harfbuzz fribidi mariadb-connector-c
 export TBB_INC=$(ls -d /usr/local/Cellar/tbb/*)/include
 export TBB_LIB=$(ls -d /usr/local/Cellar/tbb/*)/lib
 
+# Cleanup old installation
+if [ -d ${R_LIBS} ]; then echo "Cleanup old r environment..." && rm -rf ${R_LIBS}; fi
+
 # Instal R packages
 echo "R library location: ${R_LIBS}"
-if [ -d ${R_LIBS} ]; then echo "Cleanup old r environment..." && rm -rf ${R_LIBS}; fi
 mkdir -p ${R_LIBS}
 # prerequisite package
 Rscript -e "install.packages(c('littler', 'docopt'), lib='${R_LIBS}', repos='${CRAN}', clean=TRUE, quiet=TRUE)"
 PATH=${R_LIBS}/littler/examples:${R_LIBS}/littler/bin:${PATH}
-# basic
-install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} -s \
-    devtools \
-    pacman \
-    renv \
-    languageserver \
-    IRkernel \
-    lintr \
-    styler \
-    formatR \
-    feather \
-    reticulate
-# tidyverse
-install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} -s \
-    tidyverse \
-    devtools \
-    rmarkdown \
-    BiocManager \
-    vroom \
-    gert
-# dplyr database backends
-install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} -s \
-    arrow \
-    dbplyr \
-    DBI \
-    dtplyr \
-    duckdb \
-    nycflights13 \
-    Lahman \
-    RMariaDB \
-    RPostgres \
-    RSQLite \
-    fst
-# report & publish
-install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} -s \
-    blogdown \
-    bookdown \
-    distill \
-    rticles \
-    rmdshower \
-    xaringan \
-    printr \
-    kableExtra
-# graphics
-install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} -s \
-    ggforce \
-    ggpmisc \
-    ggExtra \
-    ggfortify \
-    ggtext \
-    gghighlight \
-    ggthemes \
-    ggsci \
-    paletteer \
-    GGally \
-    ggridges \
-    ggraph \
-    lemon \
-    cowplot \
-    ggpubr \
-    corrplot \
-    ggbeeswarm \
-    ggThemeAssist \
-    shades \
-    visreg \
-    magick \
-    tidygraph \
-    wesanderson \
-    MetBrewer \
-    showtext
-Rscript -e "remotes::install_github('jorvlan/raincloudplots', lib='${R_LIBS}', clean=TRUE, quiet=TRUE)"
-# statistic
-install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} -s \
-    afex \
-    emmeans \
-    psych \
-    coin \
-    robustbase \
-    Rmisc \
-    lme4 \
-    lmerTest \
-    mediation \
-    brms \
-    jmv \
-    sjPlot \
-    finalfit \
-    cocor \
-    skimr \
-    broom.mixed
-# other
-install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} -s \
-    Rmpi \
-    vitae \
-    rorcid \
-    RNifti \
-    R.matlab \
-    fMRIscrub
+
+# Read R packages list (CRAN)
+R_PKG_FILE="${SCRIPT_DIR}/../environment_spec/r_environment.txt"
+# read the file line by line and filter out lines starting with # or whitespace
+R_PKG_LIST=""
+echo -e "\n\nInstalling R packages:\n"
+while IFS= read -r line; do
+    if [[ "${line}" =~ ^[^[:space:]#] && ! -z "${line}" ]]; then
+        R_PKG_LIST="${R_PKG_LIST}${line} "
+        echo ${line}
+    fi
+done < "${R_PKG_FILE}"
+# deal with bad last line (without a newline in the end)
+if [[ "${line}" =~ ^[^[:space:]#] && ! -z "${line}" ]]; then
+    R_PKG_LIST="${R_PKG_LIST}${line} "
+    echo ${line}
+fi
+# Install R packages
+install2.r --error -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} -s ${R_PKG_LIST}
+
+# Read R packages list (Github)
+R_PKG_FILE="${SCRIPT_DIR}/../environment_spec/r_environment_github.txt"
+# read the file line by line and filter out lines starting with # or whitespace
+R_PKG_LIST=""
+while IFS= read -r line; do
+    if [[ "${line}" =~ ^[^[:space:]#] && ! -z "${line}" ]]; then
+        R_PKG_LIST="${R_PKG_LIST}${line} "
+    fi
+done < "${R_PKG_FILE}"
+# deal with bad last line (without a newline in the end)
+if [[ "${line}" =~ ^[^[:space:]#] && ! -z "${line}" ]]; then
+    R_PKG_LIST="${R_PKG_LIST}${line} "
+fi
+# Install R packages (from Github)
+if [ ! -z "${R_PKG_LIST}" ]; then
+    echo -e "\n\nInstalling R packages (from Github):\n"
+    echo ${R_PKG_LIST}
+    installGithub.r -d TRUE -r ${CRAN} ${R_PKG_LIST}
+fi
 
 # Setup IRkernel for jupyter
 Rscript -e "IRkernel::installspec()"
+
 # Write ~/.Renviron
 cat >~/.Renviron <<EOL
 R_LIBS=${R_LIBS}
