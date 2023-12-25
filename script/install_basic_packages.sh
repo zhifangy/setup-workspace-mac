5d3e83344bbacca307466e26b55802d8dcce4794
Zhifang Ye <zhifang.ye.fghm@gmail.com>
2023-12-25 14:07:49 -0800

    [EHN] Add package starship [ENH] Check homebrew installation before running installation script [ENH] Better .zshrc helper output

--- script/install_basic_packages.sh
+++ script/install_basic_packages.sh
@@ -2,19 +2,38 @@
 set -e
 
 # Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+if [ ! -d /opt/homebrew ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
 
 # Install packages
 # basic
 brew install wget curl vim cmake gcc llvm tcl-tk libssh2 open-mpi openblas node swig v8 hdf5 \
     imagemagick sevenzip
 # useful utilities
-brew install git htop tree pandoc autossh bat lsd thefuck orbstack
+brew install git htop tree pandoc autossh starship bat lsd thefuck orbstack
 brew install gromgit/fuse/sshfs-mac macfuse
 
 # Add following lines into .zshrc
 echo "
 Add following line to .zshrc
+
+# Homebrew
+export PATH=\"/opt/homebrew/bin:\${PATH}\"
+# Homebrew zsh completions
+if type brew &>/dev/null
+then
+    FPATH=\"\$(brew --prefix)/share/zsh/site-functions:\${FPATH}\"
+
+    autoload -Uz compinit
+    compinit
+fi
+
+# Starship
+eval \"\$(starship init zsh)\"
+
+# The fuck
+eval \"\$(thefuck --alias)\"
 
 # Alias
 # lsd
@@ -24,6 +43,4 @@ alias lla=\"lsd -la\"
 alias lt=\"lsd --tree\"
 # bat
 alias cat=\"bat\"
-
-eval $(thefuck --alias)
 "
