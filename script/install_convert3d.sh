80fa4ac0c9dc6272d56ac8b8c6687eb5c3ef3a53
Zhifang Ye <zhifang.ye.fghm@gmail.com>
2023-12-26 22:50:55 -0800

    [ENH] Update Convert3D to aarch64 build

--- script/install_convert3d.sh
+++ script/install_convert3d.sh
@@ -12,15 +12,15 @@ if [ -d ${C3D_DIR} ]; then rm -rf ${C3D_DIR}; fi
 # Install
 echo "Installing Convert3D from SourceForge..."
 mkdir -p ${C3D_DIR}
-wget -q https://downloads.sourceforge.net/project/c3d/c3d/Experimental/c3d-${C3D_VERSION}-MacOS-x86_64.dmg \
+wget -q https://downloads.sourceforge.net/project/c3d/c3d/Experimental/c3d-${C3D_VERSION}-MacOS-arm64.dmg \
     -P ${C3D_DIR}
-7zz x ${C3D_DIR}/c3d-${C3D_VERSION}-MacOS-x86_64.dmg -o"${C3D_DIR}/" \
-    c3d-${C3D_VERSION}-MacOS-x86_64/Convert3DGUI.app > /dev/null
-mv ${C3D_DIR}/c3d-${C3D_VERSION}-MacOS-x86_64/Convert3DGUI.app ${C3D_DIR}/Convert3DGUI.app
+7zz x ${C3D_DIR}/c3d-${C3D_VERSION}-MacOS-arm64.dmg -o"${C3D_DIR}/" \
+    c3d-${C3D_VERSION}-MacOS-arm64/Convert3DGUI.app > /dev/null
+mv ${C3D_DIR}/c3d-${C3D_VERSION}-MacOS-arm64/Convert3DGUI.app ${C3D_DIR}/Convert3DGUI.app
 
 # Cleanup
-rm ${C3D_DIR}/c3d-${C3D_VERSION}-MacOS-x86_64.dmg
-rm -r ${C3D_DIR}/c3d-${C3D_VERSION}-MacOS-x86_64
+rm ${C3D_DIR}/c3d-${C3D_VERSION}-MacOS-arm64.dmg
+rm -r ${C3D_DIR}/c3d-${C3D_VERSION}-MacOS-arm64
 
 # Add following lines into .zshrc
 echo "
