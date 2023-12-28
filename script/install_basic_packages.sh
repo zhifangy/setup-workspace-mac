f8717c3c8190128a647d2a1412350ef38d23333b
Zhifang Ye <zhifang.ye.fghm@gmail.com>
2023-12-28 13:43:54 -0800

    [ENH] Add fzf package [ENH] Setup bat theme via environment variable [ENH] New alias llsize and preview

--- script/install_basic_packages.sh
+++ script/install_basic_packages.sh
@@ -11,7 +11,7 @@ fi
 brew install wget curl vim cmake gcc llvm tcl-tk libssh2 open-mpi openblas node swig v8 hdf5 \
     imagemagick sevenzip
 # useful utilities
-brew install git htop tree pandoc autossh starship bat lsd thefuck
+brew install git htop tree pandoc autossh starship fzf bat lsd thefuck
 brew install gromgit/fuse/sshfs-mac macfuse
 # latest python and r
 brew install python r
@@ -35,6 +35,14 @@ fi
 # Starship
 eval \"\$(starship init zsh)\"
 
+# bat
+export BAT_THEME=\"Dracula\"
+
+# fzf
+export PATH=\"/opt/homebrew/opt/fzf/bin:\${PATH}\"
+source \"/opt/homebrew/opt/fzf/shell/completion.zsh\"
+source \"/opt/homebrew/opt/fzf/shell/key-bindings.zsh\"
+
 # The fuck
 eval \"\$(thefuck --alias)\"
 
@@ -44,6 +52,7 @@ alias ll=\"lsd -l\"
 alias la=\"lsd -a\"
 alias lla=\"lsd -la\"
 alias lt=\"lsd --tree\"
-# bat
-alias cat=\"bat\"
+alias llsize=\"lsd -l --total-size\"
+# fzf
+alias preview=\"fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'\"
 "
