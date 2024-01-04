487d4add42929b38abe2551366d8e2c960074bb9
Zhifang <zhifang.ye.fghm@gmail.com>
2024-01-03 20:31:18 -0800

    [ENH] Install micromamba and poetry via homebrew [MNT] Better .zshrc helper output

--- script/install_python_environment.sh
+++ script/install_python_environment.sh
@@ -9,31 +9,23 @@ MAMBA_DIR=${SETUP_ROOT}/micromamba
 POETRY_HOME=${SETUP_ROOT}/poetry
 PY_LIBS=${SETUP_ROOT}/pyenv
 
-# Cleanup old installation
-if [ ! -z "$(command -v micromamba)" ]; then
-    if [ $(micromamba env list | grep -c ${PY_LIBS}) -ne 0 ]; then
-        echo "Cleanup old environment ${PY_LIBS}..."
-        micromamba env remove -p ${PY_LIBS} -yq
-    fi
-else
-    rm -rf ${PY_LIBS}
-fi
-if [ -d ${MAMBA_DIR} ]; then echo "Cleanup old micromamba installation..." && rm -rf ${MAMBA_DIR}; fi
-if [ -d ${POETRY_HOME} ]; then echo "Cleanup old poetry installation..." && rm -rf ${POETRY_HOME}; fi
-
-# Install Micromamba
-mkdir -p ${MAMBA_DIR} && curl -Ls https://micro.mamba.pm/api/micromamba/osx-64/latest | \
-    tar -C ${MAMBA_DIR} -xvj bin/micromamba
-echo "Current mamba: $(which mamba)"
-export PATH=${MAMBA_DIR}/bin:${PATH}
-echo "Current mamba: $(which micromamba)"
-
-# Install Poetry
+# Install Micromamba, Poetry
+brew install micromamba poetry
+echo "Current micromamba: $(which micromamba)"
+echo "Current poetry: $(which poetry)"
+export MAMBA_ROOT_PREFIX
 export POETRY_HOME
 export POETRY_CACHE_DIR=${POETRY_HOME}
 export POETRY_CONFIG_DIR=${POETRY_HOME}
-curl -sSL https://install.python-poetry.org | python3 -
-export PATH=${POETRY_HOME}/bin:${PATH}
+
+# Cleanup old python environment
+if [ $(micromamba env list | grep -c ${PY_LIBS}) -ne 0 ]; then
+    echo "Cleanup old environment ${PY_LIBS}..."
+    micromamba env remove -p ${PY_LIBS} -yq
+elif [ -d ${PY_LIBS} ]; then
+    echo "Cleanup old environment ${PY_LIBS}..."
+    rm -rf ${PY_LIBS}
+fi
 
 # Create python environment
 echo "Python enviromenmet location: ${PY_LIBS}"
@@ -60,12 +52,10 @@ echo "
 Add following lines to .zshrc:
 
 # Micromamba
-export MAMBA_DIR=${MAMBA_DIR}
-export PATH=\${MAMBA_DIR}/bin:\${PATH}
 # >>> mamba initialize >>>
 # !! Contents within this block are managed by 'mamba init' !!
-export MAMBA_EXE=\${MAMBA_DIR}/bin/micromamba;
-export MAMBA_ROOT_PREFIX=\${MAMBA_DIR};
+export MAMBA_EXE=/usr/local/bin/micromamba;
+export MAMBA_ROOT_PREFIX=${SETUP_ROOT}/micromamba;
 __mamba_setup=\"\$(\"\$MAMBA_EXE\" shell hook --shell zsh --root-prefix \"\$MAMBA_ROOT_PREFIX\" 2> /dev/null)\"
 if [ \$? -eq 0 ]; then
     eval \"\$__mamba_setup\"
@@ -79,7 +69,6 @@ unset __mamba_setup
 export POETRY_HOME=${SETUP_ROOT}/poetry
 export POETRY_CACHE_DIR=\${POETRY_HOME}
 export POETRY_CONFIG_DIR=\${POETRY_HOME}
-export PATH=\${POETRY_HOME}/bin:\${PATH}
 
 # Activate python environment
 export PY_LIBS=${PY_LIBS}
