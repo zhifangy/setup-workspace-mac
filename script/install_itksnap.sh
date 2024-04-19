a46387a3fbe420a36666688082790054ea0f5ca7
Zhifang Ye <zhifang.ye.fghm@gmail.com>
2024-04-19 16:55:31 -0700

    [ENH] Update ITK-SNAP to 4.2.0

--- script/install_itksnap.sh
+++ script/install_itksnap.sh
@@ -4,8 +4,8 @@ set -e
 # Setup
 source $( dirname -- "$( readlink -f -- "$0"; )"; )/../envs
 ITKSNAP_DIR=${SETUP_ROOT}/neurotools/itksnap
-ITKSNAP_VERSION=${ITKSNAP_VERSION:-4.0.2}
-ITKSNAP_DATE=${ITKSNAP_DATE:-20230925}
+ITKSNAP_VERSION=${ITKSNAP_VERSION:-4.2.0}
+ITKSNAP_DATE=${ITKSNAP_DATE:-20240422}
 
 # Cleanup old installation
 if [ -d ${ITKSNAP_DIR} ]; then rm -rf ${ITKSNAP_DIR}; fi
@@ -13,9 +13,9 @@ if [ -d ${ITKSNAP_DIR} ]; then rm -rf ${ITKSNAP_DIR}; fi
 # Install
 echo "Installing ITK-SNAP from SourceForge..."
 mkdir -p ${ITKSNAP_DIR}
-wget -q https://sourceforge.net/projects/itk-snap/files/itk-snap/${ITKSNAP_VERSION}/itksnap-${ITKSNAP_VERSION}-Darwin-arm64.dmg \
+wget  https://sourceforge.net/projects/itk-snap/files/itk-snap/${ITKSNAP_VERSION}/itksnap-${ITKSNAP_VERSION}-${ITKSNAP_DATE}-Darwin-arm64.dmg \
     -P ${ITKSNAP_DIR}
-7zz x ${ITKSNAP_DIR}/itksnap-${ITKSNAP_VERSION}-Darwin-arm64.dmg -o"${ITKSNAP_DIR}/" \
+7zz x ${ITKSNAP_DIR}/itksnap-${ITKSNAP_VERSION}-${ITKSNAP_DATE}-Darwin-arm64.dmg -o"${ITKSNAP_DIR}/" \
     itksnap-${ITKSNAP_VERSION}-${ITKSNAP_DATE}-Darwin-arm64/ITK-SNAP.app > /dev/null
 mv ${ITKSNAP_DIR}/itksnap-${ITKSNAP_VERSION}-${ITKSNAP_DATE}-Darwin-arm64/ITK-SNAP.app ${ITKSNAP_DIR}/ITK-SNAP.app
 
@@ -24,7 +24,7 @@ if [[ -d /Applications/ITK-SNAP.app || -L /Applications/ITK-SNAP.app ]]; then rm
 ln -s ${ITKSNAP_DIR}/ITK-SNAP.app /Applications/ITK-SNAP.app
 
 # Cleanup
-rm ${ITKSNAP_DIR}/itksnap-${ITKSNAP_VERSION}-Darwin-arm64.dmg
+rm ${ITKSNAP_DIR}/itksnap-${ITKSNAP_VERSION}-${ITKSNAP_DATE}-Darwin-arm64.dmg
 rm -r ${ITKSNAP_DIR}/itksnap-${ITKSNAP_VERSION}-${ITKSNAP_DATE}-Darwin-arm64
 
 # Add following lines into .zshrc
