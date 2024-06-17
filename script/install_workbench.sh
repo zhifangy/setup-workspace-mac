75ed5aa221e1703d747067e20810cba271dddbc5
Zhifang <zhifang.ye.fghm@gmail.com>
2024-06-17 15:06:06 -0700

    [ENH] Update Workbench to 2.0.0

--- script/install_workbench.sh
+++ script/install_workbench.sh
@@ -5,32 +5,28 @@ set -e
 source $( dirname -- "$( readlink -f -- "$0"; )"; )/../envs
 BASE_DIR=${SETUP_ROOT}/neurotools
 WORKBENCH_DIR=${SETUP_ROOT}/neurotools/workbench
-WORKBENCH_VERSION=${WORKBENCH_VERSION:-v1.5.0}
+WORKBENCH_VERSION=${WORKBENCH_VERSION:-v2.0.0}
 
 # Cleanup old installation
 if [ -d ${WORKBENCH_DIR} ]; then rm -rf ${WORKBENCH_DIR}; fi
 
 # Install
 echo "Installing Workbench from humanconnectome.org..."
-wget -q https://www.humanconnectome.org/storage/app/media/workbench/workbench-mac64-${WORKBENCH_VERSION}.zip \
+wget -q https://www.humanconnectome.org/storage/app/media/workbench/workbench-macub-${WORKBENCH_VERSION}.zip \
     -P ${BASE_DIR}
-unzip -q -d ${BASE_DIR} ${BASE_DIR}/workbench-mac64-${WORKBENCH_VERSION}.zip
-# fix permission
-chmod 755 ${WORKBENCH_DIR}/bin_macosx64 ${WORKBENCH_DIR}/macosx64_apps
-chmod 755 ${WORKBENCH_DIR}/bin_macosx64/* ${WORKBENCH_DIR}/macosx64_apps/*
-chmod 644 ${WORKBENCH_DIR}/README.txt
+unzip -q -d ${BASE_DIR} ${BASE_DIR}/workbench-macub-${WORKBENCH_VERSION}.zip
 
 # Put app to /Applications folder
 if [[ -d /Applications/Workbench.app || -L /Applications/Workbench.app ]]; then rm /Applications/Workbench.app; fi
-ln -s ${WORKBENCH_DIR}/macosx64_apps/wb_view.app /Applications/Workbench.app
+ln -s ${WORKBENCH_DIR}/macosxub_apps/wb_view.app /Applications/Workbench.app
 
 # Cleanup
-rm ${BASE_DIR}/workbench-mac64-${WORKBENCH_VERSION}.zip
+rm ${BASE_DIR}/workbench-macub-${WORKBENCH_VERSION}.zip
 
 # Add following lines into .zshrc
 echo "
 Add following lines to .zshrc:
 
 # HCP Workbench
-export PATH=${WORKBENCH_DIR}/bin_macosx64:\${PATH}
+export PATH=${WORKBENCH_DIR}/bin_macosxub:\${PATH}
 "
