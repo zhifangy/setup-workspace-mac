e54942179440111a477f90279829e21479eb9e4d
Zhifang Ye <zhifang.ye.fghm@gmail.com>
2024-11-04 15:55:02 -0800

    [ENH] Use pak package to install R packages [ENH] Reformat R package list into TOML format [ENH] Automaticaly add some environment variables into .Renviron file

--- script/install_r_environment.sh
+++ script/install_r_environment.sh
@@ -5,74 +5,75 @@ set -e
 source $( dirname -- "$( readlink -f -- "$0"; )"; )/../envs
 SCRIPT_DIR=$( dirname -- "$( readlink -f -- "$0"; )"; )
 N_CPUS=${N_CPUS:-8}
-export R_LIBS=${SETUP_ROOT}/renv
-CRAN=${CRAN:-https://packagemanager.posit.co/cran/latest}
+export \
+    R_LIBS=${SETUP_ROOT}/renv \
+    CRAN=${CRAN:-https://packagemanager.posit.co/cran/latest}
+export \
+    PKG_CRAN_MIRROR=$CRAN \
+    PKG_LIBRARY=$R_LIBS \
+    R_PKG_CACHE_DIR=${R_LIBS}/_pkgcache
 
 # Cleanup old installation
 if [ -d ${R_LIBS} ]; then echo "Cleanup old r environment..." && rm -rf ${R_LIBS}; fi
 
-# Instal R packages
+# Prepare environment
 echo "R enviromenmet location: ${R_LIBS}"
 mkdir -p ${R_LIBS}
 # prerequisite package
-Rscript -e "install.packages('docopt', lib='${R_LIBS}', repos='${CRAN}', clean=TRUE)"
-Rscript -e "install.packages('littler', lib='${R_LIBS}', repos='${CRAN}', type='source', clean=TRUE)"
-bash ${SCRIPT_DIR}/fix_littler_macos.sh
-PATH=${PATH}:${R_LIBS}/littler/examples:${R_LIBS}/littler/bin
+Rscript -e "install.packages(c('pak', 'RcppTOML'), lib='${R_LIBS}', repos='${CRAN}', clean=TRUE)"
 
-# Install R packages (from Github)
-PKG_FILE="${SCRIPT_DIR}/../environment_spec/r_environment.txt"
-PKG_LIST=""
-PKG_NO_BIN_LIST=""=""
-# read file line by line, filtering out lines that start with # or whitespace
-while IFS= read -r line || [[ -n "$line" ]]; do
-    if [[ "${line}" =~ ^[^[:space:]#] ]]; then
-        PKG_LIST+="${line} "
-        echo ${line}
-    fi
-done < "${PKG_FILE}"
-# install R packages using littler install2.r script
-# if the preferred package type is binary, the packages without precompiled binaries
-# will be captured and installed from source
-echo -e "\n\nInstalling R packages:\n"
-while IFS= read -r line; do
-    echo "$line"
-    # Check if line contains the "no binary available" message
-    if [[ "${line}" == *"not available as a binary package for this version of R"* ]]; then
-        PKG_NO_BIN_LIST=$(echo "$line" | grep -o "‘[^’]*’" | sed "s/[‘’]//g")
-        # # Extract package name from the line
-        # PKG=$(echo "${line}" | awk -F"‘|’" '{print $2}')
-        # PKG_NO_BIN_LIST+="${PKG} "
-    fi
-done < <(install2.r -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} -s ${PKG_LIST} "$@" 2>&1)
-# install packages from source
-if [[ ! -z "${PKG_NO_BIN_LIST}" ]]; then
-    install2.r -l ${R_LIBS} -n ${N_CPUS} -r ${CRAN} -t "source" -s ${PKG_NO_BIN_LIST}
-    echo -e "\nPackages installed from source:\n${PKG_NO_BIN_LIST}"
-fi
+# Install R package from TOML file
+PKG_FILE="${SCRIPT_DIR}/../environment_spec/renv.toml"
 
-# Install R packages (from Github)
-PKG_GH_FILE="${SCRIPT_DIR}/../environment_spec/r_environment_github.txt"
-PKG_GH_LIST=""
-# read file line by line, filtering out lines that start with # or whitespace
-while IFS= read -r line || [[ -n "$line" ]]; do
-    if [[ "${line}" =~ ^[^[:space:]#] ]]; then
-        PKG_GH_LIST+="${line} "
-    fi
-done < "${PKG_GH_FILE}"
-# install R packages using littler installGithub.r script
-if [ ! -z "${PKG_GH_LIST}" ]; then
-    echo -e "\n\nInstalling R packages (from Github):\n${PKG_GH_LIST}"
-    installGithub.r -d TRUE -r ${CRAN} ${PKG_GH_LIST}
-fi
+# Execute the R code with Rscript -e
+Rscript --no-environ --no-init-file -e "
+options(Ncpus=${N_CPUS})
+# Parse the TOML file and extract packages
+spec <- RcppTOML::parseTOML('${PKG_FILE}');
+# Install packages
+cat('\nPackages to install from CRAN or other repositories:\n');
+cat(unlist(spec\$packages), sep = '\\n', '\n');
+pak::pkg_install(unlist(spec\$packages), lib=\"${R_LIBS}\");
+# Cleanup cache
+pak::cache_clean()
+"
 
 # Setup IRkernel for jupyter
 Rscript -e "IRkernel::installspec()"
 
-# Write ~/.Renviron
-cat >~/.Renviron <<EOL
-R_LIBS=${R_LIBS}
-EOL
+# Add default settings to .Renviron
+RENVIRON_PATH="${HOME}/.Renviron"
+RENVIRON=$(cat <<EOF
+R_LIBS=\${R_LIBS:-${SETUP_ROOT}/renv}
+CRAN=\${CRAN:-https://packagemanager.posit.co/cran/latest}
+# pak config
+PKG_LIBRARY=\${PKG_LIBRARY:-\${R_LIBS}}
+PKG_CRAN_MIRROR=\${PKG_CRAN_MIRROR:-\${CRAN}}
+DEFAULT_R_PKG_CACHE_DIR=\${R_LIBS}/_pkgcache
+R_PKG_CACHE_DIR=\${R_PKG_CACHE_DIR:-\${DEFAULT_R_PKG_CACHE_DIR}}
+EOF
+)
+# check if .Rprofile exists
+if [ -f "$RENVIRON_PATH" ]; then
+    # prompt the user for confirmation to overwrite
+    read -p $'\n'"$RENVIRON_PATH exists. Do you want to overwrite it? (y/n): " choice
+    case "$choice" in
+        y|Y )
+            echo "$RENVIRON" > "$RENVIRON_PATH"
+            echo "$RENVIRON_PATH has been overwritten with the defaults."
+        ;;
+        n|N )
+            echo "No changes made to $RENVIRON_PATH."
+        ;;
+        * )
+            echo "Invalid input. No changes made."
+        ;;
+    esac
+else
+    # if .Renviron doesn't exist, create it with the defaults
+    echo -e "\n$RENVIRON_PATH created with the defaults."
+    echo "$RENVIRON" > "$RENVIRON_PATH"
+fi
 
 # Add following lines into .zshrc
 echo "
@@ -80,8 +81,10 @@ Add following lines to .zshrc:
 
 # R environment
 export R_LIBS=${R_LIBS}
-# cran
 export CRAN=${CRAN}
-# littler
-export PATH=\${PATH}:\${R_LIBS}/littler/examples:\${R_LIBS}/littler/bin
+# pak config
+export \\
+    PKG_LIBRARY=\${PKG_LIBRARY:-\$R_LIBS} \\
+    PKG_CRAN_MIRROR=\${PKG_CRAN_MIRROR:-\$CRAN} \\
+    R_PKG_CACHE_DIR=\${R_PKG_CACHE_DIR:-\${R_LIBS}/_pkgcache}
 "
