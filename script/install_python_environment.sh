20185e4164c5692185dd23cde698d1a8a1fa021d
Zhifang Ye <zhifang.ye.fghm@gmail.com>
2024-11-09 12:58:17 -0800

    [ENH] Optimize python environment installation script [ENH] Remove requirment of env file, use SETUP_PREFIX environment by default

--- script/install_python_environment.sh
+++ script/install_python_environment.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 set -e
 
-# Setup
-source $( dirname -- "$( readlink -f -- "$0"; )"; )/../envs
-SCRIPT_DIR=$( dirname -- "$( readlink -f -- "$0"; )"; )
-# python related
-export MAMBA_ROOT_PREFIX=${SETUP_ROOT}/micromamba
-export UV_BASE_DIR=${SETUP_ROOT}/uv
-export UV_PYTHON_INSTALL_DIR=${UV_BASE_DIR}/python
-export UV_TOOL_DIR=${UV_BASE_DIR}/tool
-export UV_CACHE_DIR=${UV_BASE_DIR}/cache
-PY_LIBS=${SETUP_ROOT}/pyenv
+# Get setup and script root directory
+if [ -z "${SETUP_PREFIX}" ]; then
+    echo "SETUP_PREFIX is not set or is empty. Defaulting to \${HOME}/Softwares."
+    export SETUP_PREFIX='${HOME}/Softwares'
+fi
+SCRIPT_ROOT_DIR=$(dirname "$(dirname "$(realpath -- "$0")")")
+# Set environment variables
+export \
+    MAMBA_ROOT_PREFIX="$(eval "echo ${SETUP_PREFIX}")/micromamba" \
+    UV_ROOT_DIR="$(eval "echo ${SETUP_PREFIX}")/uv"
+export \
+    UV_PYTHON_INSTALL_DIR="${UV_ROOT_DIR}/python" \
+    UV_TOOL_DIR="${UV_ROOT_DIR}/tool" \
+    UV_CACHE_DIR="${UV_ROOT_DIR}/cache"
+export PY_LIBS="$(eval "echo ${SETUP_PREFIX}/pyenv")"
+export PYTHON_VERSION="3.12"
 
 # Install packages
-brew install micromamba uv ruff
+formula_packages=("micromamba" "uv" "ruff")
+for package in "${formula_packages[@]}"; do
+    brew list --formula "${package}" &> /dev/null || brew install "${package}"
+done
 
 # Cleanup old python environment
 if [ $(micromamba env list | grep -c ${PY_LIBS}) -ne 0 ]; then
@@ -24,22 +33,21 @@ elif [ -d ${PY_LIBS} ]; then
     rm -rf ${PY_LIBS}
 fi
 
-# Create environment
-echo "Python enviromenmet location: ${PY_LIBS}"
-cd "$(dirname "$0")"
-# create empty environment via micromamba
+# Install uv-managed python
+uv python install ${PYTHON_VERSION}
+
+# Create python environment
 micromamba create -yq -p ${PY_LIBS}
-# activate environment
 eval "$(micromamba shell hook --shell bash --root-prefix ${MAMBA_ROOT_PREFIX})"
 micromamba activate ${PY_LIBS}
 # create venv (PEP 405 compliant) via UV in the environment directory
-uv venv --allow-existing --seed ${PY_LIBS}
+uv venv --allow-existing --seed --python ${PYTHON_VERSION} ${PY_LIBS}
 
 # Install packages in venv via UV
-uv pip install -r ${SCRIPT_DIR}/../environment_spec/pyproject.toml --extra full
+uv pip install -r ${SCRIPT_ROOT_DIR}/environment_spec/pyproject.toml --extra full
 # special treatment for brainstat
-uv pip install -r ${SCRIPT_DIR}/../environment_spec/pyproject.toml --extra brainstat_deps
-uv pip install -r ${SCRIPT_DIR}/../environment_spec/pyproject.toml --extra brainstat_nodeps --no-deps
+uv pip install -r ${SCRIPT_ROOT_DIR}/environment_spec/pyproject.toml --extra brainstat_deps
+uv pip install -r ${SCRIPT_ROOT_DIR}/environment_spec/pyproject.toml --extra brainstat_nodeps --no-deps
 
 # Cleanup
 micromamba clean -apyq
@@ -52,8 +60,8 @@ Add following lines to .zshrc:
 # Micromamba
 # >>> mamba initialize >>>
 # !! Contents within this block are managed by 'mamba init' !!
-export MAMBA_EXE=${HOMEBREW_ROOT}/bin/micromamba;
-export MAMBA_ROOT_PREFIX=${MAMBA_ROOT_PREFIX};
+export MAMBA_EXE=\"\$(brew --prefix micromamba)/bin/micromamba\";
+export MAMBA_ROOT_PREFIX=\"${SETUP_PREFIX}/micromamba\";
 __mamba_setup=\"\$(\"\$MAMBA_EXE\" shell hook --shell zsh --root-prefix \"\$MAMBA_ROOT_PREFIX\" 2> /dev/null)\"
 if [ \$? -eq 0 ]; then
     eval \"\$__mamba_setup\"
@@ -63,18 +71,16 @@ fi
 unset __mamba_setup
 # <<< mamba initialize <<<
 
-# Activate python environment
-export PY_LIBS=${PY_LIBS}
-micromamba activate \${PY_LIBS}
-
 # UV
-export UV_BASE_DIR=${UV_BASE_DIR}
-export UV_PYTHON_INSTALL_DIR=\${UV_BASE_DIR}/python
-export UV_TOOL_DIR=\${UV_BASE_DIR}/tool
-export UV_CACHE_DIR=\${UV_BASE_DIR}/cache
+export UV_ROOT_DIR=\"${SETUP_PREFIX}/uv\"
+export \\
+    UV_PYTHON_INSTALL_DIR=\"\${UV_ROOT_DIR}/python\" \\
+    UV_TOOL_DIR=\"\${UV_ROOT_DIR}/tool\" \\
+    UV_CACHE_DIR=\"\${UV_ROOT_DIR}/cache\"
 
-Add below to alias section
-alias mamba=\"micromamba\"
+# Python environment
+export PY_LIBS=\"${SETUP_PREFIX}/pyenv\"
+micromamba activate \$PY_LIBS
 
 Execute following lines:
 source ~/.zshrc
