13160e8089a5bd428451cca8617b673295dced7f
Zhifang Ye <zhifang.ye.fghm@gmail.com>
2024-11-09 13:25:18 -0800

    [ENH] Revise CRAN R installation and add alias to start R without saving

--- script/install_r_cran.sh
+++ script/install_r_cran.sh
@@ -4,31 +4,11 @@ set -e
 # Install R
 brew list --cask r &> /dev/null || brew install --cask r
 
-# Add default settings to .Rprofile
-RPROFILE_PATH="${HOME}/.Rprofile"
-OPTIONS="options(
-    save.workspace = \"no\",
-    restore.workspace = \"no\"
-)
-formals(quit)\$save <- formals(q)\$save <- \"no\""
-# check if .Rprofile exists
-if [ -f "$RPROFILE_PATH" ]; then
-    # prompt the user for confirmation to overwrite
-    read -p "$RPROFILE_PATH exists. Do you want to overwrite it? (y/n): " choice
-    case "$choice" in
-        y|Y )
-            echo "$OPTIONS" > "$RPROFILE_PATH"
-            echo "$RPROFILE_PATH has been overwritten with the default options."
-        ;;
-        n|N )
-            echo "No changes made to $RPROFILE_PATH."
-        ;;
-        * )
-            echo "Invalid input. No changes made."
-        ;;
-    esac
-else
-    # if .Rprofile doesn't exist, create it with the default options
-    echo "$RPROFILE_PATH created with the default options."
-    echo "$OPTIONS" > "$RPROFILE_PATH"
-fi
+# Add following lines into .zshrc
+echo "
+Add following lines to .zshrc:
+
+# R
+export R_EXE=\"/usr/local/bin/R\"
+alias R=\"R --no-save --no-restore\"
+"
