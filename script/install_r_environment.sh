d0b8e2e05dd88926be463287a551ed44963f27e7
Zhifang Ye <zhifang.ye.fghm@gmail.com>
2024-11-09 12:59:29 -0800

    [ENH] Optimize r environment installation script [ENH] Remove requirment of env file, use SETUP_PREFIX environment by default

--- script/install_r_environment.sh
+++ script/install_r_environment.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 set -e
 
-# Setup
-source $( dirname -- "$( readlink -f -- "$0"; )"; )/../envs
-SCRIPT_DIR=$( dirname -- "$( readlink -f -- "$0"; )"; )
-N_CPUS=${N_CPUS:-8}
+# Get setup and script root directory
+if [ -z "${SETUP_PREFIX}" ]; then
+    echo "SETUP_PREFIX is not set or is empty. Defaulting to \${HOME}/Softwares."
+    export SETUP_PREFIX='${HOME}/Softwares'
+fi
+SCRIPT_ROOT_DIR=$(dirname "$(dirname "$(realpath -- "$0")")")
+# Set environment variables
 export \
-    R_LIBS=${SETUP_ROOT}/renv \
-    CRAN=${CRAN:-https://packagemanager.posit.co/cran/latest}
+    N_CPUS="${N_CPUS:-8}" \
+    CRAN=${CRAN:-https://packagemanager.posit.co/cran/latest} \
+    R_LIBS="$(eval "echo ${SETUP_PREFIX}/renv")"
 export \
-    PKG_CRAN_MIRROR=$CRAN \
-    PKG_LIBRARY=$R_LIBS \
-    R_PKG_CACHE_DIR=${R_LIBS}/_pkgcache
+    PKG_CRAN_MIRROR="$CRAN" \
+    PKG_LIBRARY="$R_LIBS" \
+    R_PKG_CACHE_DIR="${R_LIBS}/_pkgcache"
 
-# Cleanup old installation
+# Cleanup old r environment
 if [ -d ${R_LIBS} ]; then echo "Cleanup old r environment..." && rm -rf ${R_LIBS}; fi
 
 # Prepare environment
@@ -23,9 +27,7 @@ mkdir -p ${R_LIBS}
 Rscript -e "install.packages(c('pak', 'RcppTOML'), lib='${R_LIBS}', repos='${CRAN}', clean=TRUE)"
 
 # Install R package from TOML file
-PKG_FILE="${SCRIPT_DIR}/../environment_spec/renv.toml"
-
-# Execute the R code with Rscript -e
+PKG_FILE="${SCRIPT_ROOT_DIR}/environment_spec/renv.toml"
 Rscript --no-environ --no-init-file -e "
 options(Ncpus=${N_CPUS})
 # Parse the TOML file and extract packages
@@ -44,13 +46,12 @@ Rscript -e "IRkernel::installspec()"
 # Add default settings to .Renviron
 RENVIRON_PATH="${HOME}/.Renviron"
 RENVIRON=$(cat <<EOF
-R_LIBS=\${R_LIBS:-${SETUP_ROOT}/renv}
+R_LIBS=\${R_LIBS:-${R_LIBS}}
 CRAN=\${CRAN:-https://packagemanager.posit.co/cran/latest}
 # pak config
-PKG_LIBRARY=\${PKG_LIBRARY:-\${R_LIBS}}
-PKG_CRAN_MIRROR=\${PKG_CRAN_MIRROR:-\${CRAN}}
-DEFAULT_R_PKG_CACHE_DIR=\${R_LIBS}/_pkgcache
-R_PKG_CACHE_DIR=\${R_PKG_CACHE_DIR:-\${DEFAULT_R_PKG_CACHE_DIR}}
+PKG_LIBRARY=\${PKG_LIBRARY:-\$R_LIBS}
+PKG_CRAN_MIRROR=\${PKG_CRAN_MIRROR:-\$CRAN}
+R_PKG_CACHE_DIR=\${R_PKG_CACHE_DIR:-${R_PKG_CACHE_DIR}}
 EOF
 )
 # check if .Rprofile exists
@@ -80,11 +81,11 @@ echo "
 Add following lines to .zshrc:
 
 # R environment
-export R_LIBS=${R_LIBS}
-export CRAN=${CRAN}
+export R_LIBS=\"${SETUP_PREFIX}/renv\"
+export CRAN=\"$CRAN\"
 # pak config
 export \\
-    PKG_LIBRARY=\${PKG_LIBRARY:-\$R_LIBS} \\
-    PKG_CRAN_MIRROR=\${PKG_CRAN_MIRROR:-\$CRAN} \\
-    R_PKG_CACHE_DIR=\${R_PKG_CACHE_DIR:-\${R_LIBS}/_pkgcache}
+    PKG_LIBRARY=\"\$R_LIBS\" \\
+    PKG_CRAN_MIRROR=\"\$CRAN\" \\
+    R_PKG_CACHE_DIR=\"\${R_LIBS}/_pkgcache\"
 "
