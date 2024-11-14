bcfcb8ca84455146e5b18420d2410a374647e414
Zhifang Ye <zhifang.ye.fghm@gmail.com>
2024-11-14 12:39:01 -0800

    [ENH] Optimize AFNI installation

--- script/install_afni.sh
+++ script/install_afni.sh
@@ -7,15 +7,16 @@ if [ -z "${SETUP_PREFIX}" ]; then
     export SETUP_PREFIX='${HOME}/Softwares'
 fi
 # Set environment variables
-N_CPUS=${N_CPUS:-6}
-AFNI_DIR="$(eval "echo ${SETUP_PREFIX}/neurotools/afni")"
-AFNI_BUILD_DIR="$(eval "echo ${SETUP_PREFIX}/neurotools/afni_build")"
-PKG_VERSION=macos_13_ARM_clang
-# r related
+INSTALL_PREFIX="$(eval "echo ${SETUP_PREFIX}/afni")"
+BUILD_DIR="$(eval "echo ${SETUP_PREFIX}/afni_build")"
+PKG_VERSION=macos_13_ARM
 R_LIBS="${R_LIBS:-$(eval "echo ${SETUP_PREFIX}/renv")}"
+N_CPUS=${N_CPUS:-8}
+export PATH="${INSTALL_PREFIX}:${PATH}"
 
 # Cleanup old installation
-if [ -d ${AFNI_DIR} ]; then echo "Cleanup old AFNI installation..." && rm -rf ${AFNI_DIR}; fi
+if [ -d ${INSTALL_PREFIX} ]; then echo "Cleanup old AFNI installation..." && rm -rf ${INSTALL_PREFIX}; fi
+if [ -d ${BUILD_DIR} ]; then echo "Cleanup old AFNI build directory..." && rm -rf ${BUILD_DIR}; fi
 if [ -d ~/.afni/help ]; then echo "Cleanup old AFNI help files ..." && rm -rf ~/.afni/help; fi
 if [ -f ~/.afnirc ]; then echo "Cleanup old AFNI rc files ..." && rm ~/.afnirc; fi
 if [ -f ~/.sumarc ]; then echo "Cleanup old SUMA rc files ..." && rm ~/.sumarc; fi
@@ -37,14 +38,12 @@ done
 for cask in "${cask_packages[@]}"; do
     brew list --cask "${cask}" &> /dev/null || brew install --cask "${cask}"
 done
+
 # Install R dependencies
-Rscript --no-environ --no-init-file -e "
+Rscript -e "
 options(Ncpus=${N_CPUS})
 # Install packages
-pkgs_list <- c(
-    'afex', 'phia', 'snow', 'nlme', 'lmerTest', 'gamm4', 'data.table',
-    'paran', 'psych', 'corrplot', 'metafor'
-)
+pkgs_list <- c('afex', 'phia', 'snow', 'nlme', 'lmerTest', 'gamm4', 'data.table', 'paran', 'psych', 'corrplot', 'metafor')
 pak::pkg_install(pkgs_list, lib=\"${R_LIBS}\");
 # Cleanup cache
 pak::cache_clean()
@@ -52,17 +51,10 @@ pak::cache_clean()
 
 # Install AFNI
 echo "Installing AFNI from source code (building for apple aarch64)..."
-# download @update.afni.binaries and do basic setup
-mkdir -p ${AFNI_DIR}
-wget -q https://afni.nimh.nih.gov/pub/dist/bin/misc/@update.afni.binaries -P ${AFNI_DIR}
-tcsh ${AFNI_DIR}/@update.afni.binaries -no_recur -package anyos_text_atlas -bindir ${AFNI_DIR}
-# prepare for building
-build_afni.py -build_root ${AFNI_BUILD_DIR} -package ${PKG_VERSION} -prep_only
-# do actual compiling
-mkdir -p ${AFNI_BUILD_DIR}/prev
-build_afni.py -build_root ${AFNI_BUILD_DIR} -package ${PKG_VERSION} -clean_root no -do_backup no
+curl -s https://afni.nimh.nih.gov/pub/dist/bin/misc/@update.afni.binaries | tcsh -s - -no_recur -package anyos_text_atlas -bindir ${INSTALL_PREFIX}
+build_afni.py -abin ${INSTALL_PREFIX} -build_root ${BUILD_DIR} -package ${PKG_VERSION}  -do_backup no
 # post installation setup
-cp ${AFNI_DIR}/AFNI.afnirc ${HOME}/.afnirc
+cp ${INSTALL_PREFIX}/AFNI.afnirc ${HOME}/.afnirc
 suma -update_env
 apsearch -update_all_afni_help
 
@@ -75,17 +67,12 @@ echo "
 Add following line to .zshrc
 
 # AFNI
-export PATH=${AFNI_DIR}:\${PATH}
-# auto-inserted by @update.afni.binaries :
-#    set up tab completion for AFNI programs
-if [ -f \$HOME/.afni/help/all_progs.COMP.zsh ]
-then
-    autoload -U +X bashcompinit && bashcompinit
-    autoload -U +X compinit && compinit -i \\
-    && source \$HOME/.afni/help/all_progs.COMP.zsh
-fi
+export AFNI_DIR=\"${SETUP_PREFIX}/afni\"
+export PATH=\"\${AFNI_DIR}:\${PATH}\"
+# command completion
+if [ -f \${HOME}/.afni/help/all_progs.COMP.zsh ]; then source \${HOME}/.afni/help/all_progs.COMP.zsh; fi
 # look for shared libraries under flat_namespace
-export DYLD_LIBRARY_PATH=\${DYLD_LIBRARY_PATH}:/opt/X11/lib/flat_namespace
+export DYLD_LIBRARY_PATH=\"\${DYLD_LIBRARY_PATH}:/opt/X11/lib/flat_namespace\"
 # do not log commands
 export AFNI_DONT_LOGFILE=YES
 "
