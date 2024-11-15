67beac5449da2e427a8d61759f3f8f7b5a592645
Zhifang Ye <zhifang.ye.fghm@gmail.com>
2024-11-14 17:06:38 -0800

    [ENH] Add zsh plugin aliases macos copyfile copypath safe-paste you-should-use zsh-bat [ENH] Optimize script

--- script/install_oh_my_zsh.sh
+++ script/install_oh_my_zsh.sh
@@ -6,19 +6,25 @@ if [ ! -d ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 # Install zsh plugin
-if [ ! -d ~/.oh-my-zsh/custom/plugins/autoupdate ]; then
+if [ ! -d ${ZSH_CUSTOM}/plugins/autoupdate ]; then
     git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autoupdate
 fi
-if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+if [ ! -d ${ZSH_CUSTOM}/plugins/zsh-autosuggestions ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
-if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+if [ ! -d ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
-if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-abbr ]; then
+if [ ! -d ${ZSH_CUSTOM}/plugins/you-should-use ]; then
+    git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
+fi
+if [ ! -d ${ZSH_CUSTOM}/plugins/zsh-bat ]; then
+    git clone https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-bat
+fi
+if [ ! -d ${ZSH_CUSTOM}/plugins/zsh-abbr ]; then
     git clone https://github.com/olets/zsh-abbr.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-abbr
 fi
-if [ ! -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]; then
+if [ ! -d ${ZSH_CUSTOM}/themes/powerlevel10k ]; then
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
@@ -26,12 +32,19 @@ fi
 echo "
 Modify .zshrc:
 
-1.Plugins
-    plugins=(git z sudo autoupdate zsh-autosuggestions zsh-syntax-highlighting zsh-abbr)
-2.Themes
+- Move the 'source $ZSH/oh-my-zsh.sh' command to the end of the .zshrc. If a program has completion files,
+    those can be added to the \$FPATH, instead of running compinit multiple times.
+- Plugins
+    plugins=(\\
+        z git sudo aliases macos copyfile copypath safe-paste \\
+        autoupdate zsh-autosuggestions zsh-syntax-highlighting you-should-use zsh-bat zsh-abbr \\
+        )
+- Themes
     ZSH_THEME=\"powerlevel10k/powerlevel10k\"
     # Run 'p10k configure' after restart the shell
-3.Fonts
+
+Note:
+- Fonts
     In order to use the powerlevel10k theme, you need to use fonts from the Nerd-fonts project
     https://github.com/ryanoasis/nerd-fonts
 "
